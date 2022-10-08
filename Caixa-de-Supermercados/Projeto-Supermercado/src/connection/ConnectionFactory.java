d0745f697a9cc62db80db58adeb6880be2d92eb9
Mattheus Martins <mattheus_cmartins@hotmail.com>
2022-10-08 16:31:11 -0300

    Connection Factory criado

---
+++ Caixa-de-Supermercados/Projeto-Supermercado/src/connection/ConnectionFactory.java
@@ -0,0 +1,60 @@
+package connection;
+
+import com.mysql.jdbc.PreparedStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ConnectionFactory {
+
+    private final String DRIVER = "com.mysql.jdbc.Driver";
+    private final String URL = "jdbc:mysql://localhost:3306/supermercado_mm";
+    private final String USER = "admin";
+    private final String PASS = "ADS@123";
+
+    public Connection getConnection() {
+        try {
+            Class.forName(DRIVER);
+            return DriverManager.getConnection(URL, USER, PASS);
+        } catch (ClassNotFoundException | SQLException ex) {
+            throw new RuntimeException("Erro na conexão! ", ex);
+        }
+
+    }
+
+    public static void closeConnection(Connection connection) {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ConnectionFactory.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static void closeConnection(Connection connection, PreparedStatement stmt) {
+        closeConnection(connection);
+
+        try {
+            if (stmt != null) {
+                stmt.close();
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ConnectionFactory.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static void closeConnection(Connection connection, PreparedStatement stmt, ResultSet rs) {
+        closeConnection(connection, stmt);
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ConnectionFactory.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
